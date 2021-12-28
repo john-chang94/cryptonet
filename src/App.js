7def66fe1584011b9fbdf29c43ab540b9022df1b
john-chang94 <j.chang94@yahoo.com>
2021-12-27 23:44:59 -0800

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Space } from "antd";
-import Navbar from './components/Navbar';
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Space } from "antd";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Cryptocurrencies from "./components/Cryptocurrencies";
 
 function App() {
   return (
@@ -10,7 +12,14 @@ function App() {
         <Navbar />
       </div>
       <div className="main">
-        
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/coins" element={<Cryptocurrencies />} />
+            </Routes>
+          </div>
+        </Layout>
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,26 +9,41 @@ import {
   FundOutlined,
 } from "@ant-design/icons/lib/icons";
 
+import icon from "../images/cryptocurrency.png";
+
 const Navbar = () => {
   return (
     <div className="nav-container">
-      <div>
+      <div className="logo-container">
+        <img src={icon} alt="cryptonet logo" />
         <h2>
-          <Link to="/">CryptoNet</Link>
+          <Link to="/" className="nav-links">CryptoNet</Link>
         </h2>
       </div>
       <Menu theme="dark">
-        <Menu.Item key={"1"} icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
+        <Menu.Item key={"1"} className="nav-links" icon={<HomeOutlined />}>
+          <Link to="/" className="nav-links">
+            Home
+          </Link>
         </Menu.Item>
-        <Menu.Item key={"2"} icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        <Menu.Item key={"2"} className="nav-links" icon={<FundOutlined />}>
+          <Link to="/coins" className="nav-links">
+            Cryptocurrencies
+          </Link>
         </Menu.Item>
-        <Menu.Item key={"3"} icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Exchanges</Link>
+        <Menu.Item
+          key={"3"}
+          className="nav-links"
+          icon={<MoneyCollectOutlined />}
+        >
+          <Link to="/exchanges" className="nav-links">
+            Exchanges
+          </Link>
         </Menu.Item>
-        <Menu.Item key={"4"} icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
+        <Menu.Item key={"4"} className="nav-links" icon={<BulbOutlined />}>
+          <Link to="/news" className="nav-links">
+            News
+          </Link>
         </Menu.Item>
       </Menu>
     </div>
